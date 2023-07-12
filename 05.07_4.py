245328b8b702ae65d46c5c47e0324582490b6224
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-07-12 15:44:25 +0400

    Add files via upload

---
+++ 05.07_4.py
@@ -0,0 +1,20 @@
+def binary_sr(ls:list, target:int):
+    x = 0
+    y = len(ls) - 1
+   
+    while x <= y:
+        center = (x+y) // 2
+        if ls[center] == target:
+            return f'dzer uzac {target} tiv@ gtnvel a {center} indeqsov'
+        elif target > ls[center]:
+            x = center + 1
+        else:
+            y = center - 1
+
+    return "Tiv@ bacakayum a listum"
+
+ls1 = [1,5,6,8,9,4,2]
+tiv = 5
+
+print(binary_sr(ls1,tiv))
+
