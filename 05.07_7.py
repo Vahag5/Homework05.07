fada8b7935beecf7992b2a0c9de45572cab26bcd
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-07-12 18:22:57 +0400

    Update 05.07_7.py

--- 05.07_7.py
+++ 05.07_7.py
@@ -1,13 +1,18 @@
-def bubble_sort(array: list):
+def bubble_sort(array, reverse=False):
     x = len(array)
     for i in range(x-1):
-        if array[i] > array[i + 1] :
-            array[i],array[i + 1] = array[i + 1],array[i]
-            bubble_sort(array)
-
+        for j in range(x-i-1):
+            if reverse:
+                if array[j] < array[j+1]:
+                    array[j], array[j+1] = array[j+1], array[j]
+            else:
+                if array[j] > array[j+1]:
+                    array[j], array[j+1] = array[j+1], array[j]
     return array
 
-
-ls = [1,5,6,7,4,2,9,10,78,4890,100]
+ls = [1, 5, 6, 7, 4, 2, 9, 10, 78, 4890, 100]
 sorted_array = bubble_sort(ls)
-print(sorted_array)  #[1, 2, 4, 5, 6, 7, 9, 10, 78, 100, 4890]
+print(sorted_array)  # [1, 2, 4, 5, 6, 7, 9, 10, 78, 100, 4890]
+
+sorted_array2 = bubble_sort(ls, reverse=True)
+print(sorted_array2)  # [4890, 100, 78, 10, 9, 7, 6, 5, 4, 2, 1]
